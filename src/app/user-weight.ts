6e56f92aeba90e1b463a76bbca3baf61cb742072
Katelientje <gerard-97@hotmail.com>
2019-12-03 15:08:36 +0100

    3-12-19

--- src/app/user-weight.ts
+++ src/app/user-weight.ts
@@ -7,11 +7,6 @@ export class UserWeight {
     public date: Date;
     public bmi: number;
     
-    constructor(value: number, user: User) {
-    this.value = value;
-    this.user = user;       
-    
-
-
+    constructor() { 
 }
 }
